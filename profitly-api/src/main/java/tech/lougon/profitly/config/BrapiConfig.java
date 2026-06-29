d968b2e9f305dab7f6ed92972fd71a0f1bbadf95
Vítor Lougon <vitor.lougon@gmail.com>
2026-06-29 20:12:14 -0300

    fix(config): raise WebClient buffer to 10MB

--- profitly-api/src/main/java/tech/lougon/profitly/config/BrapiConfig.java
+++ profitly-api/src/main/java/tech/lougon/profitly/config/BrapiConfig.java
@@ -3,6 +3,7 @@ package tech.lougon.profitly.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -15,9 +16,14 @@ public class BrapiConfig {
 
     @Bean
     public WebClient brapiWebClient() {
+        ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(c -> c.defaultCodecs().maxInMemorySize(10 * 1024 * 1024))
+                .build();
+
         return WebClient.builder()
                 .baseUrl(baseUrl)
                 .defaultHeader("Authorization", "Bearer " + token)
+                .exchangeStrategies(strategies)
                 .build();
     }
 }
