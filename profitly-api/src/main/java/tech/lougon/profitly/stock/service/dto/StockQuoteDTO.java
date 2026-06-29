99bbb2c614dfc0d446a3b9ef5be50e8ebbe9e10b
Vítor Lougon <vitor.lougon@gmail.com>
2026-06-29 10:38:55 -0300

    feat(stock): add StockDTO in the service layer

---
+++ profitly-api/src/main/java/tech/lougon/profitly/stock/service/dto/StockQuoteDTO.java
@@ -0,0 +1,30 @@
+package tech.lougon.profitly.stock.service.dto;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public record StockQuoteDTO(
+        String requestedSymbol
+        , String symbol
+        , Boolean changed
+        , Instant requestedAt
+        , Long took
+        , String shortName
+        , String longName
+        , String currency
+        , BigDecimal regularMarketPrice
+        , BigDecimal regularMarketDayHigh
+        , BigDecimal regularMarketDayLow
+        , String regularMarketDayRange
+        , BigDecimal regularMarketChange
+        , BigDecimal regularMarketChangePercent
+        , Instant regularMarketTime
+        , Long marketCap
+        , Long regularMarketVolume
+        , BigDecimal regularMarketPreviousClose
+        , BigDecimal regularMarketOpen
+        , String fiftyTwoWeekRange
+        , BigDecimal fiftyTwoWeekLow
+        , BigDecimal fiftyTwoWeekHigh
+        , String logoUrl
+) {}
