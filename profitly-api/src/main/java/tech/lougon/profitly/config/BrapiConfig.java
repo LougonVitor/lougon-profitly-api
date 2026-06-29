96881c76eaebc4e6c8a5e8d87811abe7c43387bd
Vítor Lougon <vitor.lougon@gmail.com>
2026-06-29 13:17:51 -0300

    fix (stock): search environments variables

--- profitly-api/src/main/java/tech/lougon/profitly/config/BrapiConfig.java
+++ profitly-api/src/main/java/tech/lougon/profitly/config/BrapiConfig.java
@@ -1,13 +1,16 @@
 package tech.lougon.profitly.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class BrapiConfig {
+    @Value("${brapi.base-url}")
     private String baseUrl;
 
+    @Value("${brapi.token}")
     private String token;
 
     @Bean
