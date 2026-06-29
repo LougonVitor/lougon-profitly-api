7f06aa9f169f0a3f5ddd56f3654fa757a01bb207
Vítor Lougon <vitor.lougon@gmail.com>
2026-06-29 08:51:27 -0300

    feat(init): scaffold profitly-api spring boot project

---
+++ profitly-api/src/main/java/tech/lougon/profitly_api/ProfitlyApiApplication.java
@@ -0,0 +1,13 @@
+package tech.lougon.profitly_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfitlyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfitlyApiApplication.class, args);
+	}
+
+}
---
+++ profitly-api/src/test/java/tech/lougon/profitly_api/ProfitlyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.lougon.profitly_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfitlyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
