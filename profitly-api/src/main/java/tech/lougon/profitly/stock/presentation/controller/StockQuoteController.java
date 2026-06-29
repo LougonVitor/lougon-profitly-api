b8ba3740d2e28464e4273b785e44e098d098c3c9
Vítor Lougon <vitor.lougon@gmail.com>
2026-06-29 17:55:45 -0300

    chore (cors): disable cors for local development

--- profitly-api/src/main/java/tech/lougon/profitly/stock/presentation/controller/StockQuoteController.java
+++ profitly-api/src/main/java/tech/lougon/profitly/stock/presentation/controller/StockQuoteController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/stocks/quote")
+@CrossOrigin(origins = "http://localhost:5173")
 public class StockQuoteController {
 
     private final StockQuoteService stockService;
