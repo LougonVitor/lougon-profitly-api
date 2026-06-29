3f563cc2159f3dad3235b46ff862217627e205ac
Vítor Lougon <vitor.lougon@gmail.com>
2026-06-29 20:45:25 -0300

    chore(wallet): remove unused AddPositionRequest

--- profitly-api/src/main/java/tech/lougon/profitly/wallet/presentation/request/AddPositionRequest.java
+++
@@ -1,9 +0,0 @@
-package tech.lougon.profitly.wallet.presentation.request;
-
-import java.math.BigDecimal;
-
-public record AddPositionRequest(
-        String ticker,
-        Integer quantity,
-        BigDecimal averagePrice
-) {}
