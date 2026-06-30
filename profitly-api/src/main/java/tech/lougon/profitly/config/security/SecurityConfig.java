c1a35bb5277f30e4d59e836cc00686229fef64e7
Vítor Lougon <vitor.lougon@gmail.com>
2026-06-30 02:48:48 -0300

    feat(security): disable CORS

--- profitly-api/src/main/java/tech/lougon/profitly/config/security/SecurityConfig.java
+++ profitly-api/src/main/java/tech/lougon/profitly/config/security/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .cors(cors -> {})
+                .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
