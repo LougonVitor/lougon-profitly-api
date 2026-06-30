94c9b63f623a44a722e4b34c0a6fed5ef7cffc0a
Vítor Lougon <vitor.lougon@gmail.com>
2026-06-29 21:03:09 -0300

    feat(user/infra): UserSecurityDetails and UserDetailsService

---
+++ profitly-api/src/main/java/tech/lougon/profitly/user/infrastructure/security/UserSecurityDetails.java
@@ -0,0 +1,37 @@
+package tech.lougon.profitly.user.infrastructure.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import tech.lougon.profitly.user.domain.model.User;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserSecurityDetails implements UserDetails {
+
+    private final User user;
+
+    public UserSecurityDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.password();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.username();
+    }
+
+    public String getUserId() {
+        return user.id();
+    }
+}
